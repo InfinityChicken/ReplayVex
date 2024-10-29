b0efc9cbf467ece3d020587e30ad5ce0b14b1d1b
InfinityChicken <8zsc2014@gmail.com>
2024-10-28 22:30:23 -0700

    I LOVE LEMLIB

---
+++ include/magic.hpp
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <cstdio>
+
+inline pros::Motor driveOne(9, pros::MotorGearset::green);
+inline pros::Motor driveTwo(9, pros::MotorGearset::green);
+
+void write();
+void writeInit();
---
+++ src/magic.cpp
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <cstdio>
+
+FILE* write = fopen("/usd/autonomous.txt", "w");
+FILE* read = fopen("/usd/autonomous.txt", "r");
+
+void writeInit() {}
+
+void write() {
+    std::string dataLine = "";
+    dataLine.append(driveOne.get_voltage() + " ");
+    dataLine.append(driveTwo.get_voltage());
+    
+    fputs(dataLine, write);
+}
+
+void writeClose() {
+    fclose(write);
+}
